84a523f8989a9823a6e86a37f7dc8dab9fc023b1
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2024-06-10 11:47:26 -0300

    File and Error Handling

--- bank.go
+++ bank.go
@@ -1,10 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const accountBalanceFile = "balance.txt"
+
+// Write file
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+}
+
+// Read file
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFile)
+	if err != nil {
+		return 1000, errors.New("failed to read file")
+	}
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+	if err != nil {
+		return 1000, errors.New("failed to parse stored balance value")
+	}
+	return balance, nil
+}
 
 func main() {
 
-	var accountBalance = 1000.0
+	var accountBalance, err = getBalanceFromFile()
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("--------------")
+		// return
+		// panic("Can't continue. Sorry!")
+	}
 
 	fmt.Println("Welcome to Go Bank!")
 
@@ -32,6 +67,7 @@ func main() {
 			}
 			accountBalance += depositAmmount
 			fmt.Println("Balance updated! New amount:", accountBalance)
+			writeBalanceToFile(accountBalance)
 		case 3:
 			fmt.Print("Withdraw ammount: ")
 			var withdrawAmmount float64
@@ -46,6 +82,7 @@ func main() {
 			}
 			accountBalance -= withdrawAmmount
 			fmt.Println("Balance updated! New amount:", accountBalance)
+			writeBalanceToFile(accountBalance)
 		default:
 			fmt.Println("Goodbye!")
 			fmt.Println("Thanks for choosing our bank :)")
