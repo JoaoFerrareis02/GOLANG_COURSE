8a1622f07b81161c7195009e9d868bb38f1dd08d
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2024-06-13 13:45:34 -0300

    Learning about Channels & Goroutines

--- main.go
+++ main.go
@@ -1,23 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func greet(phrase string, doneChan chan bool) {
+	fmt.Println("Hello!", phrase)
+	doneChan <- true
+}
+
+func slowGreet(phrase string, doneChan chan bool) {
+	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
+	fmt.Println("Hello!", phrase)
+	doneChan <- true
+	// close(doneChan) // Nessesary for 2º way. This will only work if you know that this is the last one to do
+}
 
 func main() {
 
-	numbers := []int{1, 2, 3, 4, 5}
+	// 1. Running Functions as Goroutines
+	// go greet("Nice to meet you!")
+	// go greet("How are you?")
+	// go slowGreet("How ... are ... you ...?")
+	// go greet("I hope you're liking the course!")
 
-	// Spliting Slices Into Parameter Values
-	sum := sumup(numbers...)
+	// 2. Introducing & Using channels
+	// done := make(chan bool)
+	// go slowGreet("How ... are ... you ...?", done)
+	// <-done
 
-	fmt.Println(sum)
+	// 3. Working with Multiple Channels & Goroutines
 
-}
+	// 1º way
 
-// Variadic Functions - Do not force to pass arrays for values
-func sumup(numbers ...int) int {
-	sum := 0
-	for _, val := range numbers {
-		sum += val
+	dones := make([]chan bool, 4)
+
+	for index := range dones {
+		dones[index] = make(chan bool)
 	}
-	return sum
+
+	go greet("Nice to meet you!", dones[0])
+	go greet("How are you?", dones[1])
+	go slowGreet("How ... are ... you ...?", dones[2])
+	go greet("I hope you're liking the course!", dones[3])
+
+	for _, done := range dones {
+		<-done
+	}
+
+	// 2º way
+
+	// done := make(chan bool)
+
+	// go greet("Nice to meet you!", done)
+	// go greet("How are you?", done)
+	// go slowGreet("How ... are ... you ...?", done)
+	// go greet("I hope you're liking the course!", done)
+
+	// for range done {
+	// }
+
 }
